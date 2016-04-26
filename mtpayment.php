a9184b49a1980cb036185968b3536dcda7cbb784
Vytenis Ščiukas <vytsci@gmail.com>
2016-04-26 09:38:54 +0300

    1.1.9

--- mtpayment.php
+++ mtpayment.php
@@ -32,7 +32,7 @@ class MTPayment extends PaymentModule
     {
         $this->name = 'mtpayment';
         $this->tab = 'payments_gateways';
-        $this->version = '1.1.8';
+        $this->version = '1.1.9';
         $this->author = 'MisterTango';
         $this->is_eu_compatible = 1;
 
